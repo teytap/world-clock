42c0f9115804f68fc1c4287d1e91813345a851da
Mehtap Tataroğlu <teytap@gmail.com>
2022-11-20 19:18:17 +0300

    headers update

--- source/app.js
+++ source/app.js
@@ -93,7 +93,21 @@ function showTime(response) {
 }
 function getCity(city) {
   let apiUrl = `http://worldtimeapi.org/api/timezone/${city}`;
-  const res = axios.get(apiUrl).then(showTime);
+  const res = axios
+    .get(
+      apiUrl,
+      {
+        crossdomain: true,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then(showTime);
 }
 
 cities.forEach((city) => {
